7ed801e116001b6f97bebca20bdb6d6fa3623876
student <student@students-mac.student.georgianc.on.ca>
2016-01-22 11:45:51 -0500

    added some questions and things

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions:[String] = ["From what is cognac made?",
+                            "What is 7x7", "What is the capital of Vermont"]
+    let answers:[String] = ["Grapes", "49", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("View Did Load")
-        print("It sure Did")
+        questionLabel.text = questions[currentQuestionIndex]
     }
+    
+    @IBOutlet weak var nextQuestionButton: UIButton!
+    @IBAction func nextQuestionClicked(sender: AnyObject) {
+    ++currentQuestionIndex
+        if currentQuestionIndex == questions.count{
+            currentQuestionIndex = 0;
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
 
-
-
-
+    }
+    
+    @IBAction func showAnswerClicked(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
+        
+    }
+    
 }
-
